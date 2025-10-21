72d87b5047ab804cec5596f7dba2f5862d7407b0
DaemonLife <beleevskij21@gmail.com>
2025-10-22 00:36:24 +0300

    disable icons in yazi

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -167,7 +167,9 @@
           }
           {
             name = "*/";
-            run = ''piper -- eza -TL=3 --color=always --icons=always --group-directories-first --no-quotes "$1"'';
+            # run = ''piper -- eza -TL=3 --color=always --icons=always --group-directories-first --no-quotes "$1"'';
+            # disable all icons
+            run = ''piper -- eza -TL=3 --color=always --group-directories-first --no-quotes "$1"'';
           }
         ];
       };
@@ -401,40 +403,46 @@
       };
 
       icon = {
-        prepend_dirs = [
-          {
-            name = "nix";
-            text = " ";
-          }
-          {
-            name = "Music";
-            text = "󰝚 ";
-          }
-          {
-            name = "Downloads";
-            text = " ";
-          }
-          {
-            name = "Documents";
-            text = "󰈙 ";
-          }
-          {
-            name = "Videos";
-            text = " ";
-          }
-          {
-            name = "Pictures";
-            text = " ";
-          }
-          {
-            name = "home";
-            text = " ";
-          }
-          {
-            name = "Public";
-            text = "󰿆 ";
-          }
-        ];
+        # disable all icons
+        globs = [];
+        dirs = [];
+        files = [];
+        exts = [];
+        conds = [];
+        # prepend_dirs = [
+        #   {
+        #     name = "nix";
+        #     text = " ";
+        #   }
+        #   {
+        #     name = "Music";
+        #     text = "󰝚 ";
+        #   }
+        #   {
+        #     name = "Downloads";
+        #     text = " ";
+        #   }
+        #   {
+        #     name = "Documents";
+        #     text = "󰈙 ";
+        #   }
+        #   {
+        #     name = "Videos";
+        #     text = " ";
+        #   }
+        #   {
+        #     name = "Pictures";
+        #     text = " ";
+        #   }
+        #   {
+        #     name = "home";
+        #     text = " ";
+        #   }
+        #   {
+        #     name = "Public";
+        #     text = "󰿆 ";
+        #   }
+        # ];
       };
     };
 
