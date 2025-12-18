9b498c6fa048bc20db4d92bd68b9fc50a92da274
DaemonLife <beleevskij21@gmail.com>
2025-12-18 21:39:33 +0300

    telegram theme fix

--- modules/telegram/telegram_base16_theme.nix
+++ modules/telegram/telegram_base16_theme.nix
@@ -3,15 +3,16 @@ with config.lib.stylix.colors; let
 
   telegram_style = ''
     // --- Main UI ---
-    windowBg: #${base00};
+    windowBg: #${base01}; // RBM menu bg
     windowFg: #${base05};
-    windowBgOver: #${base01}; // hover bg in menu
+    windowBgOver: #${base00}; // hover bg in menu and big separator
     windowFgOver: #${base06}; // hover fg in menu
-    menuBg: #${base00}; // global bg popup animation after click
+    menuBg: #${base01}; // global bg popup animation after click
     windowFgActive: #${base00}; // fg for some light buttons
+    topBarBg: #${base00}; // chat top bg
 
     // some boxes in ui
-    boxBg: windowBg;
+    boxBg: windowBg; 
     boxTextFg: windowFg;
     boxTextFgGood: #${base0B};
     boxTextFgError: #${base08};
@@ -20,9 +21,17 @@ with config.lib.stylix.colors; let
     boxTitleAdditionalFg: #${base06};
     boxTitleCloseFg: #${base05};
     boxTitleCloseFgOver: #${base06};
+    titleFgtitleFgActive: #${base06};
+
+    // media bottom right menu (stupid again)
+    mediaviewMenuFg: #${base05}; 
+    groupCallMembersFg: #${base05}; 
+    groupCallMenuBg: #${base01}; 
+    groupCallMenuBgOver: #${base02}; 
+    groupCallMenuBgRipple: #${base03}; 
 
     // up /\ and down \/ arrow for chat and chat list
-    historyToDownBg: #${base01}7f;
+    historyToDownBg: #${base00};
     historyToDownBgOver: #${base01};
     historyToDownBgRipple: #${base0D};
     historyToDownFg: #${base05};
@@ -49,21 +58,21 @@ with config.lib.stylix.colors; let
     walletTitleButtonCloseFgActiveOver: #${base08};
 
     // --- RBM menu ---
-    menuSeparatorFg: #${base01};
+    menuSeparatorFg: windowBgOver; // separator
     windowBgRipple: #${base02}; // RBM emoji arrow and some animations bg
-    menuBgRipple: #${base00};
+    menuBgRipple: #${base01};
     menuIconFg: #${base05};
     menuIconFgOver: #${base06};
 
     // --- Main settings menu ---
-    mainMenuBg: #${base00};
-    mainMenuCoverBg: #${base01};
-    mainMenuCoverFg: #${base05};
+    mainMenuBg: windowBg;
+    mainMenuCoverBg: windowBgOver;
+    mainMenuCoverFg: windowFgOver;
     checkboxFg: #${base05}; // checkbox and radio buttons
     sliderBgInactive: #${base01}; // slider (notifications and sounds)
-    sliderBgActive: #${base06};
+    sliderBgActivewindowSubTextFg: #${base06};
 
-    windowSubTextFg: #${base05};
+    windowSubTextFg: #${base03};
     windowSubTextFgOver: #${base06};
     windowBoldFg: #${base05}; // fg text in some menu
     windowBoldFgOver: #${base06}; // hover fg text in some menu
@@ -74,42 +83,37 @@ with config.lib.stylix.colors; let
 
     // --- Program window title ---
     titleBg: #${base00};
-    titleFg: #${base05};
 
-    titleFgActive: titleFg;
     titleBgActive: titleBg;
     titleButtonBg: titleBg;
-    titleButtonFg: titleFg;
+    titleButtonFg: #${base05};
     titleButtonBgOver: #${base01};
-    titleButtonFgOver: titleFg;
+    titleButtonFgOver: #${base05};
     titleButtonBgActive: titleBgActive;
-    titleButtonFgActive: titleFgActive;
+    titleButtonFgActive: #${base05};
     titleButtonBgActiveOver: titleButtonBgOver;
-    titleButtonFgActiveOver: titleFgActive;
+    titleButtonFgActiveOver: #${base05};
     titleButtonCloseBg: titleBg;
-    titleButtonCloseFg: titleFg;
+    titleButtonCloseFg: #${base05};
     titleButtonCloseBgOver: #${base08};
     titleButtonCloseFgOver: #${base00};
     titleButtonCloseBgActive: titleBgActive;
-    titleButtonCloseFgActive: titleFgActive;
+    titleButtonCloseFgActive: #${base05};
     titleButtonCloseBgActiveOver: titleButtonCloseBgOver;
-    titleButtonCloseFgActiveOver: titleButtonCloseFgOver;
+    titleButtonCloseFgActiveOverscrollBarBg: #${base05};
 
-    // --- Scroll in CHAT LIST ---
-    // bar itself
-    scrollBarBg: #${base04};
-    scrollBarBgOver: #${base05};
-    // only bg
-    scrollBg: #${base01};
-    scrollBgOver: #${base02};
+    // --- Scroll in CHATS ---
+    scrollBg: #${base00};
+    scrollBgOver: #${base03};
 
     // --- Scroll in CHAT ---
-    // bar itself
-    historyScrollBarBg: scrollBarBg;
-    historyScrollBarBgOver: scrollBarBgOver;
-    // only bg
-    historyScrollBg: scrollBg;
-    historyScrollBgOver: scrollBgOver;
+    historyScrollBarBgOverhistoryScrollBg: #${base01};
+    historyScrollBg: #${base02};
+    historyScrollBgOver: #${base03};
+    scrollBgOverhistoryScrollBarBg: #${base05}; 
+    scrollBarBgOverscrollBg: #${base05};
+    historyScrollBarBg: #${base00};
+    historyScrollBarBgOver: #${base06};
 
     // --- Media player ---
     windowBgActive: #${base0D}; // player progress line fg in chat and (NO WAY!!!...) sliders color, blue default
@@ -119,21 +123,19 @@ with config.lib.stylix.colors; let
     mediaPlayerBg: #${base00};
     mediaPlayerActiveFg: #${base05};
     mediaPlayerInactiveFg: #${base04};
-    mediaPlayerDisabledFg: #${base03};
+    mediaPlayerDisabledFgradialFg: #${base03};
 
     // --- Media in full screen ---
     // loading icon
-    radialFg: #${base05};
-    radialBg: #${base01};
+    radialBgmediaviewMenuFg: #${base01};
 
-    mediaviewMenuFg: #${base05}; // menu text fg
     mediaviewMenuBgOver: #${base03}; // statick bg of speed line
     mediaviewBg: #${base00};
     mediaviewVideoBg: mediaviewBg;
-    mediaviewControlFg: mediaviewMenuFg;
+    mediaviewControlFg: #${base05};
 
     // caption for photo
-    mediaviewCaptionBg: #${base00}aa;
+    mediaviewCaptionBg: #${base01}aa;
     mediaviewCaptionFg: #${base05};
     mediaviewTextLinkFg: #${base0D};
 
@@ -155,11 +157,11 @@ with config.lib.stylix.colors; let
 
     // --- Effects ---
     titleShadow: #00000000;
-    windowShadowFg: #0000007f;
-    windowShadowFgFallback: #0000007f;
-    shadowFg: #${base00}; // borders color
-    slideFadeOutBg: #0000007f;
-    slideFadeOutShadowFg: #0000007f;
+    windowShadowFg: #00000000;
+    windowShadowFgFallback: #00000000;
+    shadowFg: #${base01}00; // borders color
+    slideFadeOutBg: #00000000;
+    slideFadeOutShadowFg: #00000000;
     imageBg: #000000ff;
     imageBgTransparent: #000000ff;
     layerBg: #00000050; // Side bar shade
@@ -192,22 +194,20 @@ with config.lib.stylix.colors; let
     // --- Message input box ---
     placeholderFg: #${base05}; // 'Wrtie a message...' text (and fg search input... IDIOTS!!!)
     placeholderFgActive: #${base04}; // 'Wrtie a message...' text active (and fg search input active..)
-    historyComposeIconFg: #${base05};
-    historyComposeIconFgOver: #${base06};
 
-    historyComposeAreaBg: #${base00};
-    historyComposeAreaFg: historyComposeIconFg;
-    historyComposeAreaFgService: historyComposeIconFg;
-    historySendIconFg: historyComposeIconFg;
-    historySendIconFgOver: historyComposeIconFgOver;
+    historyComposeIconFgOverhistoryComposeAreaBg: #${base00};
+    historyComposeAreaFg: #${base04}; // pinned msg text and input fg (stupid)
+    historyComposeAreaFgService: #${base04}; // pinned media fg
+    historySendIconFg: #${base01};
+    historySendIconFgOver: #${base01};
     historyPinnedBg: #${base00};
-    historyReplyBg: historyComposeAreaBg;
-    historyReplyIconFg: historyComposeIconFg;
-    historyReplyCancelFg: historyComposeIconFg;
-    historyReplyCancelFgOver: historyComposeIconFgOver;
+    historyReplyBg: #${base00};
+    historyReplyIconFg: #${base01};
+    historyReplyCancelFg: #${base01};
+    historyReplyCancelFgOver: #${base01};
 
     // --- Popup when hover message's clock ---
-    tooltipBg: #${base00};
+    tooltipBg: #${base01};
     tooltipFg: #${base05};
     tooltipBorderFg: #${base01};
 
@@ -222,18 +222,18 @@ with config.lib.stylix.colors; let
     contactsBg: #${base00};
     contactsBgOver: windowBgOver;
     contactsNameFg: boxTextFg;
-    contactsStatusFg: windowSubTextFg;
+    contactsStatusFg: #${base05};
     contactsStatusFgOver: windowSubTextFgOver;
     contactsStatusFgOnline: #${base05};
 
     // --- Photo editor ---
     photoCropFadeBg: #000000aa;
-    photoCropPointFg: #${base02}7f;
+    photoCropPointFg: #${base04}7f;
 
     // --- Login menu? ---
     introBg: #${base00};
     introTitleFg: #${base05};
-    introDescriptionFg: windowSubTextFg;
+    introDescriptionFg: #${base05};
     introErrorFg: #${base05};
     introCoverTopBg: #${base00};
     introCoverBottomBg: #${base00};
@@ -250,6 +250,7 @@ with config.lib.stylix.colors; let
 
     // --- List of chats, channels and bots ---
     dialogsBg: #${base00};
+    dialogsBgActive: #${base0C};
     dialogsNameFg: #${base05};
     dialogsChatIconFg: dialogsNameFg;
     dialogsDateFg: dialogsNameFg;
@@ -263,39 +264,37 @@ with config.lib.stylix.colors; let
 
     // chat msg counter icon
     dialogsUnreadBg: #${base0D};
-    dialogsUnreadBgMuted: #${base04};
-    dialogsUnreadFg: #${base00};
 
-    dialogsTextFgOver: #${base06};
-    dialogsBgOver: #${base01};
-    dialogsTextFgServiceOver: dialogsTextFgOver;
-    dialogsNameFgOver: dialogsTextFgOver;
-    dialogsChatIconFgOver: dialogsTextFgOver;
-    dialogsDateFgOver: dialogsTextFgOver;
-    dialogsDraftFgOver: dialogsTextFgOver;
+    dialogsTextFgServiceOver: #${base06};
+    dialogsNameFgOver: #${base06};
+    dialogsChatIconFgOver: #${base06};
+    dialogsDateFgOver: #${base06};
+    dialogsDraftFgOver: #${base06};
     dialogsVerifiedIconBgOver: dialogsVerifiedIconBg;
     dialogsVerifiedIconFgOver: dialogsVerifiedIconFg;
     dialogsSendingIconFgOver: dialogsSendingIconFg;
     dialogsSentIconFgOver: dialogsSentIconFg;
     dialogsUnreadBgOver: dialogsUnreadBg;
-    dialogsUnreadBgMutedOver: dialogsUnreadBgMuted;
-    dialogsUnreadFgOver: dialogsUnreadFg;
+    dialogsUnreadBgMuteddialogsBgOver: #${base03};
+    dialogsUnreadBgMutedOverdialogsUnreadFgdialogsTextFgOver: #${base06};
+    dialogsUnreadFgdialogsTextFgOver: #${base05};
 
-    dialogsBgActive: #${base02};
-    dialogsNameFgActive: #${base06};
-    dialogsChatIconFgActive: #${base06};
-    dialogsDateFgActive: #${base06};
-    dialogsTextFgActive: #${base06};
-    dialogsTextFgServiceActive: #${base06};
-    dialogsDraftFgActive: #${base06};
-    dialogsVerifiedIconBgActive: #${base06};
+    dialogsUnreadFgOverdialogsBgActive: #${base02};
+    dialogsNameFgActive: #${base00};
+    dialogsChatIconFgActive: #${base00};
+    dialogsDateFgActive: #${base00};
+    dialogsTextFgActive: #${base00};
+    dialogsTextFgServiceActive: #${base00};
+    dialogsDraftFgActive: #${base00};
+    dialogsVerifiedIconBgActive: #${base05};
     dialogsVerifiedIconFgActive: dialogsVerifiedIconFg;
     dialogsSendingIconFgActive: dialogsSendingIconFg;
-    dialogsSentIconFgActive: #${base06};
+    dialogsSentIconFgActive: #${base00};
+    dialogsUnreadFgActive: #${base01};
     dialogsUnreadBgActive: #${base06};
-    dialogsUnreadBgMutedActive: dialogsUnreadBgMuted;
-    dialogsUnreadFgActive: dialogsUnreadFg;
-    dialogsForwardBg: dialogsBgActive;
+    dialogsUnreadBgMuted: #${base03};
+    dialogsUnreadBgMutedActive: #${base03};
+    dialogsForwardBg: #${base02};
     dialogsForwardFg: dialogsNameFgActive;
 
     // --- Search ---
@@ -311,19 +310,17 @@ with config.lib.stylix.colors; let
     emojiPanHeaderFg: #${base05};
     emojiPanHeaderBg: #${base01}; // it does not works...
     emojiIconFg: #${base05}; // emoji category fg
-    stickerPreviewBg: #${base00}7f; // full screen bg
+    stickerPreviewBg: #2020207f; // full screen bg
     stickerPanDeleteBg: #${base01}; // delete icon for sticker
     stickerPanDeleteFg: #${base05};
 
     // --- In chat ---
-    topBarBg: #${base00}; // chat top bar
 
-    reportSpamBg: #${base00};
     reportSpamFg: #${base05};
 
     // Media in msg wheh msg selected
-    msgSelectOverlay: #${base0D}7f;
-    msgStickerOverlay: #${base0D}7f;
+    msgSelectOverlay: #${base02}7f;
+    msgStickerOverlay: #${base02}7f;
 
     // sound icon in msg media
     historyFileThumbIconFg: #${base05};
@@ -340,7 +337,7 @@ with config.lib.stylix.colors; let
 
     // date on image in msg
     msgDateImgFg: #${base05};
-    msgDateImgBg: #${base01}7f;
+    msgDateImgBg: #${base00}7f;
     msgDateImgBgOver: msgDateImgBg;
     msgDateImgBgSelected: #${base01};
 
@@ -359,15 +356,13 @@ with config.lib.stylix.colors; let
     msgInDateFg: historyTextInFg;
     msgInMonoFg: historyTextInFg;
     msgInReplyBarColor: historyTextInFg;
-    msgInReplyBarSelColor: historyTextInFg;
 
     historyFileInIconFg: msgInBg;
     historyFileInRadialFg: msgInBg;
     msgFileThumbLinkInFg: lightButtonFg;
     msgFileInBg: #${base05};
-    msgFileInBgOver: #${base06};
+    msgFile4BgSelectedyoutubePlayIconBg: #${base06};
 
-    msgWaveformInActive: #${base0D};
     msgWaveformInInactive: #${base02};
     mediaInFg: msgInDateFg;
     historyLinkInFg: #${base0E};
@@ -394,24 +389,23 @@ with config.lib.stylix.colors; let
     msgOutDateFg: historyTextOutFg;
     msgOutServiceFg: historyTextOutFg;
     msgOutMonoFg: historyTextOutFg;
-    msgOutReplyBarColor: historyTextOutFg;
+    msgOutReplyBarColorhistoryFileOutIconFg: #${base00};
+    msgOutBgSelectedhistoryPeerUserpicFg: #${base00}; 
 
-    historyFileOutIconFg: msgOutBg;
     historyFileOutRadialFg: msgOutBg;
     msgFileThumbLinkOutFg: #${base0D}; // 'OPEN WITH' file text
     msgFileOutBg: #${base00};
-    msgFileOutBgOver: #${base01};
+    msgFileInBgOvermsgWaveformInActive: #${base01};
 
-    msgWaveformOutActive: msgWaveformInActive;
-    msgWaveformOutInactive: msgWaveformInInactive;
+    msgFileOutBgOvermsgWaveformOutActive: #${base0D};
+    msgWaveformOutInactivemediaOutFg: #${base0D};
 
-    mediaOutFg: historyTextOutFg;
     historyLinkOutFg: #${base0E};
 
     // selected like in
     historyTextOutFgSelected: historyTextInFgSelected;
     historyOutIconFgSelected: historyTextInFgSelected;
-    msgOutReplyBarSelColor: msgInReplyBarSelColor;
+    msgOutReplyBarSelColor: #${base05};
     historySendingOutIconFg: historyTextInFgSelected;
     msgFileThumbLinkOutFgSelected: historyTextInFgSelected;
     msgFileOutBgSelected: msgFileInBgSelected;
@@ -422,11 +416,10 @@ with config.lib.stylix.colors; let
     historyFileOutRadialFgSelected: historyFilenInRadialFgSelected;
     msgOutServiceFgSelected: msgInServiceFgSelected;
     msgOutDateFgSelected: msgInDateFgSelected;
-    msgOutBgSelected: msgInBgSelected;
+    msgInReplyBarSelColorhistoryFileInIconFg: #${base00};
 
 
     // --- User withoin avatar ---
-    historyPeerUserpicFg: #${base01}; // default color
     historyPeer1NameFg: #${base08}; // red
     historyPeer1UserpicBg: #${base08};
     historyPeer2NameFg: #${base0B}; // green
@@ -460,19 +453,16 @@ with config.lib.stylix.colors; let
     msgFile4Bg: #${base01}; // yellow
     msgFile4BgDark: #${base0A};
     msgFile4BgOver: #${base04};
-    msgFile4BgSelected: #${base04};
 
-    youtubePlayIconBg: #${base00}80; // youtube
     youtubePlayIconFg: #${base08};
     videoPlayIconBg: #${base00}80; // other video services
-    videoPlayIconFg: #${base0D};
+    topBarBgreportSpamBg: #${base00};
 
-    toastBg: #${base00}; // for link t.me ?
     toastFg: #${base05};
 
     // --- Side bar ---
     sideBarBg: #${base00};
-    sideBarBgActive: sideBarBg;
+    sideBarBgActive: #${base00};
     sideBarIconFg: #${base04};
     sideBarIconFgActive: #${base0C};
     sideBarBadgeBg: sideBarIconFgActive;
@@ -499,38 +489,39 @@ with config.lib.stylix.colors; let
     historyUnreadBarBg: #${base00};
     historyUnreadBarBorder: shadowFg;
     historyUnreadBarFg: #${base05};
-    historyForwardChooseBg: #${base03}80;
+    historyForwardChooseBg: #${base01};
     historyForwardChooseFg: #${base05};
     msgImgReplyBarColor: #${base05};
-    overviewCheckBg: #${base03}80;
+    overviewCheckBg: #${base01};
     overviewCheckFg: #${base04};
     overviewCheckFgActive: #${base01};
-    overviewPhotoSelectOverlay: #bdbdbd4a;
+    overviewPhotoSelectOverlay: #${base02};
     profileStatusFgOver: #${base04};
     profileVerifiedCheckBg: #${base05};
     profileVerifiedCheckFg: #${base01};
     profileAdminStartFg: #${base05};
     notificationsBoxMonitorFg: #${base05};
-    notificationsBoxScreenBg: windowSubTextFg;
+    notificationsBoxScreenBg: #${base05};
     notificationSampleUserpicFg: #${base01};
     notificationSampleCloseFg: #${base05};
     notificationSampleTextFg: #${base05};
     notificationSampleNameFg: #${base04};
     mediaviewFileBg: #000000;
     mediaviewFileNameFg: #${base05};
-    mediaviewFileSizeFg: windowSubTextFg;
+    mediaviewFileSizeFg: #${base05};
     mediaviewFileRedCornerFg: #${base08};
     mediaviewFileYellowCornerFg: #${base0A};
     mediaviewFileGreenCornerFg: #${base0B};
     mediaviewFileBlueCornerFg: #${base0D};
     mediaviewFileExtFg: activeButtonFg;
-    mediaviewMenuBg: #${base00};
+    mediaviewMenuBg: #${base01};
     mediaviewMenuBgRipple: menuBgRipple;
-    mediaviewControlBg: #${base07}80;
+    mediaviewControlBg: #${base05};
     mainMenuCloudBg: #${base03};
     overviewCheckBorder: #${base08};
     dialogsOnlineBadgeFgActive: #${base08};
     menuBgOver: #${base02};
+
   '';
 in
 {
@@ -539,9 +530,9 @@ in
     # create theme
     cd $HOME/.config && echo "${telegram_style}" > tmp_colors.tdesktop-theme;
     sed '/^#/d' tmp_colors.tdesktop-theme > colors.tdesktop-theme;
-    ${pkgs.imagemagick}/bin/magick -size 2960x2960 xc:#${base02} background.jpg;
-    ${pkgs.zip}/bin/zip telegram-base16.zip background.jpg colors.tdesktop-theme;
+    ${pkgs.imagemagick}/bin/magick -size 512x512 xc:#${base02} -depth 8 -colorspace sRGB -quality 100 tiled.jpg;
+    ${pkgs.zip}/bin/zip telegram-base16.zip tiled.jpg colors.tdesktop-theme;
     mv telegram-base16.zip telegram-base16.tdesktop-theme;
-    rm -rf colors.tdesktop-theme tmp_colors.tdesktop-theme background.jpg;
+    rm -rf colors.tdesktop-theme tmp_colors.tdesktop-theme tiled.jpg;
   '';
 }
